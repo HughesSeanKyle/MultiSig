f9618018906aaab77110cd6b43d86da1e7a9c752
HughesSeanKyle <khughessean@yahoo.com>
2021-03-08 14:34:46 +0200

    Test Addition - Fallback

--- test/multi-sig-wallet.js
+++ test/multi-sig-wallet.js
@@ -15,37 +15,56 @@ contract("MultiSigWallet", accounts => {
       wallet = await MultiSigWallet.new(owners, NUM_CONFIRMATIONS_REQUIRED);
   });
 
-  // Testing Contructor Function
+  // Testing Constructor Function
+  /*
   describe("constructor", () => {
     it("should deploy a contract", async () => { 
-
+ 
       for (let i = 0; i < owners.length; i++) {
         assert.equal(await wallet.owners(i), owners[i])
       }
-
+ 
       assert.equal(
         await wallet.numConfirmationsRequired(),
         NUM_CONFIRMATIONS_REQUIRED
       )
-    }) 
+    }); 
     
     it("should reject if no owners", async () => {
       await expect(MultiSigWallet.new([], NUM_CONFIRMATIONS_REQUIRED)).to.be
         .rejected
-    })
-
-    it("should reject if num conf required > owners", async () => {
+    });
+ 
+    it("should reject if number of confrmations required > owners", async () => {
       await expect(MultiSigWallet.new(owners, owners.length + 1)).to.be.rejected
-    })
-
+    });
+ 
     it("should reject if owners not unique", async () => {
       await expect(
         MultiSigWallet.new([owners[0], owners[0]], NUM_CONFIRMATIONS_REQUIRED)
       ).to.be.rejected
-    })
-
+    });
   });
+  */
 
+  // Testing Fallback Function
+  /*
+  describe("fallback", async () => {
+    it("should receive ether", async () => {
+      const { logs } = await wallet.sendTransaction({
+        from: accounts[0],
+        value: 1,
+      })
+
+      assert.equal(logs[0].event, "Deposit")
+      assert.equal(logs[0].args.sender, accounts[0])
+      assert.equal(logs[0].args.amount, 1)
+      assert.equal(logs[0].args.balance, 1)
+    })
+  })
+  */
+
+  // Testing Contructor Function
   /*
     // Testing execute Trx Function
   describe("executeTransaction", () => {
