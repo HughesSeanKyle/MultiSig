1e12462d0b4f5592436acabe7f7da40f2d1657b9
HughesSeanKyle <khughessean@yahoo.com>
2021-03-12 13:09:29 +0200

    Setting up frontend, Installing necessary dependencies and setting up metamask with web3

---
+++ client/multi-sig-wallet/src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Message, Button } from 'semantic-ui-react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const account = "0x123654";
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Multi Sig Wallet</h1>
+        <div>Account: {account}</div>
+
+        <Message warning>Metamask is not connected</Message>
+        <Button
+          color="green"
+        >
+          Connect to Metamask
+        </Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
